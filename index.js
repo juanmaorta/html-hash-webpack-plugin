41fe66ef41db0c2efaec9060c6147ca4dab0f0dd
Egor H <goga@knopka.com>
2016-10-10 17:06:49 +0500

    replacing several chunks of one entry. Replacing links to files of any type. Matching any tags, not only <script>

--- index.js
+++ index.js
@@ -12,24 +12,39 @@ function HtmlHashPlugin(options) {
     this.settings = Object.assign(defaults, options);
 };
 
-HtmlHashPlugin.prototype.apply = function(compiler) {
+HtmlHashPlugin.prototype.apply = function (compiler) {
     var settings = this.settings;
 
     // statsData seems to be a global variable
-    compiler.plugin('done', function(statsData) {
+    compiler.plugin('done', function (statsData) {
         var stats = statsData.toJson();
 
         if (!stats.errors.length) {
             var htmlFileName = settings.html_file,
                 html = FileSystem.readFileSync(path.join(settings.origin, htmlFileName), 'utf8'),
                 htmlOutput = html,
-                re;
+                re,
+                chunkNames,
+                ext;
 
-            for(chunk in stats.assetsByChunkName) {
-                re = new RegExp('<script src=(.*)(' + chunk + ')\.js(.*)', 'i');
-                htmlOutput = htmlOutput.replace(re, function(full, prefix, chunk, postfix) {
-                    return '<script src=' + prefix + stats.assetsByChunkName[chunk] + postfix;
-                });
+            var publicPath = stats.publicPath;
+            if (publicPath[0] == "/")
+                publicPath = publicPath.substr(1);
+
+            for (chunk in stats.assetsByChunkName) {
+                chunkNames = stats.assetsByChunkName[chunk];
+                if (!Array.isArray(chunkNames))
+                    chunkNames = [chunkNames];
+
+                for (var i = 0; i < chunkNames.length; i++) {
+                    var name = chunkNames[i];
+
+                    ext = name.substr(name.lastIndexOf('.') + 1);
+                    re = new RegExp('(=[\'"]/?' + publicPath + ')(' + chunk + ')\.' + ext + '([\'"])', "i");
+                    htmlOutput = htmlOutput.replace(re, function (full, prefix, chunk, postfix) {
+                        return prefix + name + postfix;
+                    });
+                }
             }
 
             FileSystem.writeFileSync(
